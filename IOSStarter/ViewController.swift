8c9756a06b946ce9cb047631b5552a952d86c90f
Nick <nsagias@gmail.com>
2023-05-01 23:08:42 -0400

    Connect Label, imageView, and change button to controller.

--- IOSStarter/ViewController.swift
+++ IOSStarter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ChameleonLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeImageClicked(_ sender: Any) {
+    }
 }
 
